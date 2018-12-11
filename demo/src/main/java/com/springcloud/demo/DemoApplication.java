d751e61506b2e3440ae65a0f7e9907f0ce68e024
Alessio Fiore <alessio.fiore@capgemini.com>
2018-12-11 15:36:40 +0100

    discovery server

--- demo/src/main/java/com/springcloud/demo/DemoApplication.java
+++ demo/src/main/java/com/springcloud/demo/DemoApplication.java
@@ -3,13 +3,13 @@ package com.springcloud.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RefreshScope
+@EnableEurekaClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
